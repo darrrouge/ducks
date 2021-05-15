3622652956c83246f7512e80a00ff073af678f31
darrrouge <darrrouge@gmail.com>
2021-05-15 14:17:41 +0300

    duck game

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿window.addEventListener("load", function () {
+  let count = 0;
+  let label = document.getElementsByClassName("duck");
+  for (let i = 0; i < label.length; i++) {
+    label[i].onclick = function () {
+      count++;
+      if (count == 3) {
+        alert("You win!");
+      }
+    };
+  }
+});
